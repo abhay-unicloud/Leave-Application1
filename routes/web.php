0543d27604c0e318232b7925fdf741d5d89d5db7
Abhay-Panwar <abhay2004panwar@gmail.com>
2024-11-01 12:38:14 +0530

    test change

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::middleware('auth.dashboard')->group(function () {
     Route::get('/admin-lockscreen', [Mycontroller::class, 'admin_lockscreen'])->name('admin_lockscreen');
     Route::get('/dashboard', [Mycontroller::class, 'index'])->name('index');
